e483c79d171e255bc08fb73506b2ca5922f0f961
Tim Spencer <72101647+sei-tspencer@users.noreply.github.com>
2022-09-01 14:29:31 -0400

    add current year to header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,3 @@
-// Copyright 2022 Carnegie Mellon University. All Rights Reserved.
-// Released under a MIT (SEI)-style license, please see LICENSE.md in the project root for license information or contact permission@sei.cmu.edu for full terms.
-
 import { OverlayContainer } from '@angular/cdk/overlay';
 import { Component, HostBinding, HostListener, OnDestroy } from '@angular/core';
 import { MatIconRegistry } from '@angular/material/icon';
