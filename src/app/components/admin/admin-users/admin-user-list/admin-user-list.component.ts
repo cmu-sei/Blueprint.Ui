aae10a93535502deff6466fedf669fa2cbcd57d8
sei-tspencer <trspencer@sei.cmu.edu>
2026-03-04 19:49:42 +0000

    admin users and groups use tables now

--- src/app/components/admin/admin-users/admin-user-list/admin-user-list.component.ts
+++ src/app/components/admin/admin-users/admin-user-list/admin-user-list.component.ts
@@ -12,19 +12,12 @@ import {
   SimpleChanges,
 } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
-import { MatSort, Sort } from '@angular/material/sort';
-import { MatPaginator, PageEvent } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
 import { ConfirmDialogComponent } from 'src/app/components/shared/confirm-dialog/confirm-dialog.component';
 import { User, SystemRole } from 'src/app/generated/blueprint.api';
-import { Observable, of } from 'rxjs';
-import { map } from 'rxjs/operators';
-import {
-  fromMatSort,
-  sortRows,
-  fromMatPaginator,
-  paginateRows,
-} from 'src/app/datasource-utils';
+import { Observable } from 'rxjs';
 import { SystemRoleDataService } from 'src/app/data/system-role/system-role-data.service';
 import { MatSelectChange } from '@angular/material/select';
 
@@ -42,19 +35,13 @@ export interface Action {
   standalone: false
 })
 export class AdminUserListComponent implements OnInit, OnChanges {
-  displayedColumns: string[] = ['id', 'name'];
+  displayedColumns: string[] = ['id', 'name', 'role'];
   filterString = '';
   savedFilterString = '';
   userDataSource = new MatTableDataSource<User>(new Array<User>());
   newUser: User = {};
 
-  // MatPaginator Output
-  pageEvent: PageEvent;
   addingNewUser: boolean;
-  displayedRows$: Observable<User[]>;
-  totalRows$: Observable<number>;
-  sortEvents$: Observable<Sort>;
-  pageEvents$: Observable<PageEvent>;
   roles$ = this.roleDataService.roles$;
   topbarColor = '#BB0000';
 
@@ -72,12 +59,11 @@ export class AdminUserListComponent implements OnInit, OnChanges {
     private roleDataService: SystemRoleDataService
   ) { }
 
-  /**
-   * Initialization
-   */
   ngOnInit() {
-    this.sortEvents$ = fromMatSort(this.sort);
-    this.pageEvents$ = fromMatPaginator(this.paginator);
+    if (this.paginator) {
+      this.userDataSource.paginator = this.paginator;
+    }
+    this.userDataSource.sort = this.sort;
     this.roleDataService.getRoles().subscribe();
     this.filterAndSort(this.filterString);
   }
@@ -89,33 +75,15 @@ export class AdminUserListComponent implements OnInit, OnChanges {
     }
   }
 
-  /**
-   * Called by UI to add a filter to the userDataSource
-   * @param filterValue
-   */
   applyFilter(filterValue: string) {
     this.filterString = filterValue.toLowerCase();
     this.filterAndSort(this.filterString);
   }
 
-  /**
-   * filters and sorts the displayed rows
-   */
   filterAndSort(filterValue: string) {
     this.userDataSource.filter = filterValue;
-    const rows$ = of(this.userDataSource.filteredData);
-    this.totalRows$ = rows$.pipe(map((rows) => rows.length));
-    if (!!this.sortEvents$ && !!this.pageEvents$) {
-      this.displayedRows$ = rows$.pipe(
-        sortRows(this.sortEvents$),
-        paginateRows(this.pageEvents$)
-      );
-    }
   }
 
-  /**
-   * Adds a new user
-   */
   addNewUser(addUser: boolean) {
     if (addUser) {
       const user = {
