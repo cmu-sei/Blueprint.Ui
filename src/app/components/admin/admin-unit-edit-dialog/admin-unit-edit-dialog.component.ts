e764214e71c834951238566b11dac5b5e87731e2
sei-tspencer <trspencer@sei.cmu.edu>
2026-07-29 15:16:00 +0000

    add description to teams and units

--- src/app/components/admin/admin-unit-edit-dialog/admin-unit-edit-dialog.component.ts
+++ src/app/components/admin/admin-unit-edit-dialog/admin-unit-edit-dialog.component.ts
@@ -5,6 +5,7 @@
 import { Component, EventEmitter, Inject, Output } from '@angular/core';
 import { UntypedFormControl, Validators } from '@angular/forms';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { AngularEditorConfig } from '@kolkov/angular-editor';
 
 @Component({
     selector: 'app-admin-unit-edit-dialog',
@@ -28,6 +29,37 @@ export class AdminUnitEditDialogComponent {
       Validators.required,
     ]
   );
+  public descriptionFormControl = new UntypedFormControl(
+    this.data.unit.description,
+    []
+  );
+  editorConfig: AngularEditorConfig = {
+    editable: true,
+    spellcheck: true,
+    height: 'auto',
+    minHeight: '0',
+    maxHeight: 'auto',
+    width: 'auto',
+    minWidth: '0',
+    translate: 'yes',
+    enableToolbar: true,
+    showToolbar: true,
+    placeholder: 'Enter text here...',
+    defaultParagraphSeparator: '',
+    defaultFontName: '',
+    defaultFontSize: '',
+    fonts: [
+      { class: 'arial', name: 'Arial' },
+      { class: 'times-new-roman', name: 'Times New Roman' },
+      { class: 'calibri', name: 'Calibri' },
+      { class: 'comic-sans-ms', name: 'Comic Sans MS' },
+    ],
+    uploadUrl: '',
+    uploadWithCredentials: false,
+    sanitize: false,
+    toolbarPosition: 'top',
+    toolbarHiddenButtons: [['backgroundColor']],
+  };
 
   constructor(
     @Inject(MAT_DIALOG_DATA) public data: any
@@ -55,6 +87,9 @@ export class AdminUnitEditDialogComponent {
       this.data.unit.shortName = this.unitShortNameFormControl.value
         .toString()
         .trim();
+      this.data.unit.description = this.descriptionFormControl.value
+        ? this.descriptionFormControl.value.toString().trim()
+        : '';
       if (this.errorFree) {
         this.editComplete.emit({
           saveChanges: saveChanges,
@@ -74,6 +109,11 @@ export class AdminUnitEditDialogComponent {
         break;
       case 'shortName':
         this.data.unit.shortName = this.unitShortNameFormControl.value.toString();
+        break;
+      case 'description':
+        this.data.unit.description = this.descriptionFormControl.value
+          ? this.descriptionFormControl.value.toString()
+          : '';
         break;
       default:
         break;
--- src/app/components/team-edit-dialog/team-edit-dialog.component.ts
+++ src/app/components/team-edit-dialog/team-edit-dialog.component.ts
@@ -5,6 +5,7 @@
 import { Component, EventEmitter, Inject, Output } from '@angular/core';
 import { UntypedFormControl, Validators } from '@angular/forms';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { AngularEditorConfig } from '@kolkov/angular-editor';
 
 const MIN_NAME_LENGTH = 3;
 
@@ -34,10 +35,41 @@ export class TeamEditDialogComponent {
     this.data.team.email,
     []
   );
+  public descriptionFormControl = new UntypedFormControl(
+    this.data.team.description,
+    []
+  );
   public citeTeamTypeIdFormControl = new UntypedFormControl(
     this.data.team.citeTeamTypeId,
     []
   );
+  editorConfig: AngularEditorConfig = {
+    editable: true,
+    spellcheck: true,
+    height: 'auto',
+    minHeight: '0',
+    maxHeight: 'auto',
+    width: 'auto',
+    minWidth: '0',
+    translate: 'yes',
+    enableToolbar: true,
+    showToolbar: true,
+    placeholder: 'Enter text here...',
+    defaultParagraphSeparator: '',
+    defaultFontName: '',
+    defaultFontSize: '',
+    fonts: [
+      { class: 'arial', name: 'Arial' },
+      { class: 'times-new-roman', name: 'Times New Roman' },
+      { class: 'calibri', name: 'Calibri' },
+      { class: 'comic-sans-ms', name: 'Comic Sans MS' },
+    ],
+    uploadUrl: '',
+    uploadWithCredentials: false,
+    sanitize: false,
+    toolbarPosition: 'top',
+    toolbarHiddenButtons: [['backgroundColor']],
+  };
 
   constructor(
     @Inject(MAT_DIALOG_DATA) public data: any
@@ -76,6 +108,9 @@ export class TeamEditDialogComponent {
       this.data.team.shortName = this.teamShortNameFormControl.value
         .toString()
         .trim();
+      this.data.team.description = this.descriptionFormControl.value
+        ? this.descriptionFormControl.value.toString().trim()
+        : '';
       if (this.errorFree) {
         this.editComplete.emit({
           saveChanges: saveChanges,
@@ -98,6 +133,9 @@ export class TeamEditDialogComponent {
         break;
       case 'email':
         this.data.team.email = this.emailFormControl.value?.toString() || '';
+        break;
+      case 'description':
+        this.data.team.description = this.descriptionFormControl.value?.toString() || '';
         break;
       case 'citeTeamTypeId':
         this.data.team.citeTeamTypeId = this.citeTeamTypeIdFormControl.value?.toString() || '';
