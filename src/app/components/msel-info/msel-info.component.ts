d777098db19d1aa10f35e686386e652a2426a479
sei-tspencer <trspencer@sei.cmu.edu>
2026-08-13 18:11:21 +0000

    cite team type required check for integrations

--- src/app/components/msel-info/msel-info.component.ts
+++ src/app/components/msel-info/msel-info.component.ts
@@ -526,16 +526,9 @@ export class MselInfoComponent implements OnDestroy, OnInit {
   citeWarningMessage() {
     let warningMessage = '';
     if (this.msel.useCite && !this.msel.citeEvaluationId) {
-      const teamsWithType = this.teamList.filter((t) => t.citeTeamTypeId);
-      const teamsWithoutType = this.teamList.filter((t) => !t.citeTeamTypeId);
-
-      if (teamsWithoutType.length > 0 && teamsWithType.length > 0) {
-        // Some teams missing types - this blocks the push
-        warningMessage = `** ERROR: ${teamsWithoutType.length} team(s) are missing a CITE Team Type. All teams must have a team type selected before pushing to CITE. **`;
-      } else if (teamsWithoutType.length > 0 && teamsWithType.length === 0) {
-        // No teams have types - warning only
-        warningMessage = '** WARNING: No teams have a CITE Team Type selected, so no teams will be pushed to CITE! **';
-      }
+      warningMessage = this.citeToDo()
+        ? '** There are unassigned CITE Team Types in Teams **'
+        : '';
     }
     return warningMessage;
   }
@@ -546,8 +539,7 @@ export class MselInfoComponent implements OnDestroy, OnInit {
     if (!this.msel.useCite || this.msel.citeEvaluationId) {
       return false; // CITE not enabled or already deployed, no validation needed
     }
-    // Check if ANY teams exist without a citeTeamTypeId
-    return this.teamList.some((t) => !t.citeTeamTypeId);
+    return this.citeToDo();
   }
 
   hasGalleryEventsWithMissingData(): boolean {
@@ -799,6 +791,13 @@ export class MselInfoComponent implements OnDestroy, OnInit {
       hasToDos = todoList.length > 0;
     }
     return hasToDos;
+  }
+
+  citeToDo(): boolean {
+    // every team on this MSEL must have a CITE team type assigned before pushing
+    return this.teamList.some(
+      (t) => t.mselId === this.msel.id && !t.citeTeamTypeId
+    );
   }
 
   startTimeCheck() {
